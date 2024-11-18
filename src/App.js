17be9c0c54cd607ad8c144b6adc9dadbc8f81385
Sdg877 <sdrakegill@gmail.com>
2024-11-18 05:53:49 +0000

    removing unused files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Check Your Horoscopes...</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
