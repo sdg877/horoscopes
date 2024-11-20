f14be916c9449103227b6988d7f26fd1929317ce
Sdg877 <sdrakegill@gmail.com>
2024-11-20 07:22:36 +0000

    Background colour added for Taurus page

---
+++ src/pages/Taurus.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'; 
+
+const Taurus = () => {
+  return (
+    <div className="taurus-page">
+
+      <h1>Welcome to the Taurus Page</h1>
+    </div>
+  );
+};
+
+export default Taurus;
