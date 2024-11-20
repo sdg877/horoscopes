3899ea7ac87e510c3113e91555b156ac3e38d358
Sdg877 <sdrakegill@gmail.com>
2024-11-20 07:32:43 +0000

    Background colour added for Scorpio page

---
+++ src/pages/Scorpio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Scorpio = () => {
+  return (
+    <div className="scorpio-page">
+
+      <h1>Welcome to the Scorpio Page</h1>
+    </div>
+  )
+}
+
+export default Scorpio
