01cebc39324c786cbfe4d33c5a305bd84fd83ca6
Sdg877 <sdrakegill@gmail.com>
2024-11-20 07:25:47 +0000

    Background colour added for Gemini page

---
+++ src/pages/Gemini.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+const Gemini = () => {
+  return (
+    <div className="gemini-page">
+
+      <h1>Welcome to the Gemini Page</h1>
+    </div>
+  )
+}
+
+export default Gemini
