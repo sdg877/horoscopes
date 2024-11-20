c69786d7714f7eebcdce9e09be10de879197d98a
Sdg877 <sdrakegill@gmail.com>
2024-11-20 07:28:10 +0000

    Background colour added for Libra page

---
+++ src/pages/Libra.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+const Libra = () => {
+  return (
+    <div className="libra-page">
+
+      <h1>Welcome to the Libra Page</h1>
+    </div>
+  )
+}
+
+export default Libra
