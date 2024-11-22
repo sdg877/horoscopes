927735d468d2f73eaf455d839378d1b7bab2e1f6
Sdg877 <sdrakegill@gmail.com>
2024-11-22 08:13:59 +0000

    Zodiac buttons implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const zodiacSigns = [
+    "Aquarius", "Aries", "Cancer", "Capricorn", "Gemini", "Leo", 
+    "Libra", "Pisces", "Sagittarius", "Scorpio", "Taurus", "Virgo"
+  ];
 
-    
   return (
-    <div>
-      
+    <div className="home-container">
+      <h1>Please select your Zodiac Sign to view your horoscope</h1>
+      <div className="controls">
+        {zodiacSigns.map((sign) => (
+          <Link to={`/${sign.toLowerCase()}`} key={sign}>
+            <button className="zodiac-button">{sign}</button>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
